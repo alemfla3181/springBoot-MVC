48a0d92ccc1b216a2f7ce9b0db3799f5c34c43db
dragoncastle <alemfla3181@naver.com>
2024-10-15 14:30:51 +0900

    Reactive Programming

--- src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -6,16 +6,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @Controller
 public class PositionController {
     @NonNull
     private final AircraftRepository repository;
+    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
 
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
-        model.addAttribute("currentPositions", repository.findAll());
+        Flux<Aircraft> aircraftFlux = repository.deleteAll()
+                .thenMany(client.get()
+                        .retrieve()
+                        .bodyToFlux(Aircraft.class)
+                        .filter(plane -> !plane.getReg().isEmpty())
+                        .flatMap(repository::save));
+
+        model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
     }
 }
--- src/main/java/com/thehecklers/aircraftpositions/PositionRetriever.java
+++ src/main/java/com/thehecklers/aircraftpositions/PositionRetriever.java
@@ -1,23 +1,40 @@
-package com.thehecklers.aircraftpositions;
-
-import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-import java.util.function.Consumer;
-
-@AllArgsConstructor
-@Configuration
-public class PositionRetriever {
-    private final AircraftRepository repository;
-
-    @Bean
-    Consumer<List<Aircraft>> retrieveAircraftPositions(){
-        return acList -> {
-            repository.deleteAll();
-            repository.saveAll(acList);
-            repository.findAll().forEach(System.out::println);
-        };
-    }
-}
+//package com.thehecklers.aircraftpositions;
+//
+//import lombok.AllArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketSession;
+//
+//import java.util.List;
+//import java.util.function.Consumer;
+//
+//@AllArgsConstructor
+//@Configuration
+//public class PositionRetriever {
+//    private final AircraftRepository repository;
+//    private final WebSocketHandler handler;
+//
+//    @Bean
+//    Consumer<List<Aircraft>> retrieveAircraftPositions(){
+//        return acList -> {
+//            repository.deleteAll();
+//            repository.saveAll(acList);
+//            repository.findAll().forEach(System.out::println);
+//        };
+//    }
+//
+//    private void sendPositions(){
+//        if(repository.count() > 0){
+//            for(WebSocketSession sessionInList : handler.getSessionList()){
+//                try{
+//                    sessionInList.sendMessage(
+//                            new TextMessage(repository.findAll().toString())
+//                    );
+//                } catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+//}
