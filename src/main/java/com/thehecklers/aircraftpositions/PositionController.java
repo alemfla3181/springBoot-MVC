e92164dba6eebec3cc45cba7e6fcb6c494bfe83e
dragoncastle <alemfla3181@naver.com>
2024-10-16 15:13:15 +0900

    RSocket

--- src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -3,18 +3,26 @@ package com.thehecklers.aircraftpositions;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Controller
 public class PositionController {
-    @NonNull
     private final AircraftRepository repository;
+    private final RSocketRequester requester;
     private WebClient client = WebClient.create("http://localhost:7634/aircraft");
+
+    public PositionController(AircraftRepository repository, RSocketRequester.Builder builder) {
+        this.repository = repository;
+        this.requester = builder.tcp("localhost", 7635);
+    }
 
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
@@ -27,5 +35,13 @@ public class PositionController {
 
         model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/acstream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Aircraft> getCurrentACPositionsStream(){
+        return requester.route("acstream")
+                .data("Requesting aircratf positions")
+                .retrieveFlux(Aircraft.class);
     }
 }
